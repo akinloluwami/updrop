c3461756a0515b79710d986d6e924ed8e027bea3
titanium-dev <akinkunmioye42@gmail.com>
2025-06-01 12:12:58 +0100

    ugh

---
+++ apps/desktop/src/main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
+
+const router = createRouter({
+  routeTree,
+  context: {},
+  defaultPreload: "intent",
+  scrollRestoration: true,
+  defaultStructuralSharing: true,
+  defaultPreloadStaleTime: 0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
+
+// Use contextBridge
+window.ipcRenderer.on("main-process-message", (_event, message) => {
+  console.log(message);
+});
