4d0224770d392741ff6f780079ba196266c93de2
titanium-dev <akinkunmioye42@gmail.com>
2025-06-01 11:28:59 +0100

    FUCK!!!!!

--- apps/desktop/src/routes/__root.tsx
+++
@@ -1,9 +0,0 @@
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-
-export const Route = createRootRoute({
-  component: () => (
-    <div className="max-w-7xl mx-auto p-6">
-      <Outlet />
-    </div>
-  ),
-});
